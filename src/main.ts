667308d565388d5e63b46fa2d0aaa6827e4ab418
Philipp Thomas <philipp.thomas@idealo.de>
2023-10-06 12:58:00 +0200

    add logging to investigate issues with bundled app

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 
 import { app } from "electron";
 import { buildTray } from "./tray-initialization";
+import  * as log from "electron-log"
+import * as path from "path";
 
 app.whenReady().then(() => {
     buildTray();
     setInterval(buildTray, 5000);
 
+    log.transports.file.resolvePath = () => path.join(app.getPath('userData'), 'logs/app.log');
+    console.log = log.log;
 });
