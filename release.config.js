2161b1949db9303bff9fa55488a97d485f690741
Philipp Thomas <philipp.thomas@idealo.de>
2024-12-20 11:19:15 +0100

    chore: initialize semantic-release

---
+++ release.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    branches: ['main'],
+    plugins: [
+        '@semantic-release/commit-analyzer',
+        '@semantic-release/release-notes-generator',
+        ["@semantic-release/git", {
+            "message": `chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.releaseNotes}`
+        }],
+        '@semantic-release/github'
+    ]
+};
