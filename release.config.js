1ecd3f607a73ce07bb3d0734c385844ef278156a
Philipp Thomas <philipp.thomas@idealo.de>
2024-12-20 11:49:35 +0100

    fix: message for release commit

--- release.config.js
+++ release.config.js
@@ -3,9 +3,8 @@ module.exports = {
     plugins: [
         '@semantic-release/commit-analyzer',
         '@semantic-release/release-notes-generator',
-        '@semantic-release/npm',
         ["@semantic-release/git", {
-            "message": `chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.releaseNotes}`
+            "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
         }],
         '@semantic-release/github'
     ]
