f84a2d0544b283f40028ed3ad5ad1ea0b578a9de
Philipp Thomas <philipp.thomas@idealo.de>
2025-01-13 12:00:38 +0100

    fix: use exec plugin to determine next release type

--- release.config.js
+++ release.config.js
@@ -3,12 +3,16 @@ module.exports = {
     plugins: [
         '@semantic-release/commit-analyzer',
         '@semantic-release/release-notes-generator',
+        ["@semantic-release/exec",
+            {
+                "verifyReleaseCmd": 'echo "releaseType=${nextRelease.type}" >> $GITHUB_OUTPUT'
+            }
+        ],
         ["@semantic-release/npm", { "npmPublish": false }],
         ["@semantic-release/git", {
             "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
         }],
-        [
-            "@semantic-release/github",
+        ["@semantic-release/github",
             {
                 "assets": [
                     { "path": "artifacts/*.dmg", "label": "cctron for macos" },
